fee4fc870dffac438a383bfcd71cf341c3fb8fab
Emanuel Arroyo <ema201217@gmail.com>
2022-08-26 04:13:29 -0300

    ---

---
+++ public/javascript/glider.config.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    dots: '#dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  })
+})
