bd58861ec340028dac53a5e9e301227becf4c327
Emanuel Arroyo <ema201217@gmail.com>
2022-08-28 11:06:26 -0300

    ---

--- public/javascript/reCaptcha.config.js
+++ public/javascript/reCaptcha.config.js
@@ -1,5 +1,3 @@
-const { localsName } = require("ejs")
-
 var onloadCallback = function () {
   grecaptcha.render('ReCaptcha', {
       sitekey: /https:\/\/cuerdas-app.herokuapp.com/.test(window.location.href) ? '6Lf31UAcAAAAABrw2DUMZXzCldUObT0L4lEoBJcP': '6LfpdrQhAAAAAMlJq4D37DZoDctHcG38mI7exiEW',
