4064668eed177cb61575ee961d593e0b7489cc43
Emanuel Arroyo <ema201217@gmail.com>
2022-08-28 11:38:05 -0300

    recaptcha add

--- public/javascript/reCaptcha.config.js
+++ public/javascript/reCaptcha.config.js
@@ -1,6 +1,6 @@
 var onloadCallback = function () {
   grecaptcha.render('ReCaptcha', {
-      sitekey: 'AIzaSyAoIZ-KDaN70pjbFczMqabnz-g2D6o_GNA',
+      sitekey: '6Lf5AbUhAAAAAErunhZQwN0ffg3q1RNCA3gwqG7Z',
       theme: 'light',
       size: 'normal',
   })
