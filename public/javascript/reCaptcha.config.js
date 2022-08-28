3225fb897b1acc2f02edc06108ea3b9363e11892
Emanuel Arroyo <ema201217@gmail.com>
2022-08-28 11:11:06 -0300

    ----|||---

--- public/javascript/reCaptcha.config.js
+++ public/javascript/reCaptcha.config.js
@@ -1,6 +1,6 @@
 var onloadCallback = function () {
   grecaptcha.render('ReCaptcha', {
-      sitekey: /https:\/\/cuerdas-app.herokuapp.com/.test(window.location.href) ? '6Lf31UAcAAAAABrw2DUMZXzCldUObT0L4lEoBJcP': '6LfpdrQhAAAAAMlJq4D37DZoDctHcG38mI7exiEW',
+      sitekey: '6LfpdrQhAAAAAMlJq4D37DZoDctHcG38mI7exiEW',
       theme: 'light',
       size: 'normal',
   })
