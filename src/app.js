61038eda3497f11c8f50d7b8970e41564b2fd15a
Emanuel Arroyo <ema201217@gmail.com>
2022-08-31 22:04:42 -0300

    sin express-partials

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const partials = require('express-partials')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,6 +20,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,7 +1,11 @@
+const { readDB } = require("../data");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 module.exports = {
   /* PRODUCTOS */
   products: (req, res) => {
-    return res.render("admin/products");
+    const products = readDB('products.json')
+    return res.render("admin/products",{products,toThousand});
   },
   edit: (req, res) => {
     return res.render("admin/product-edit");
@@ -9,17 +13,20 @@ module.exports = {
 
   /* CATEGORIAS */
   categories: (req, res) => {
-    return res.render("admin/categories");
+    const categories = readDB('categories.json')
+    return res.render("admin/categories",{categories});
   },
 
   /* USUARIOS */
   users: (req, res) => {
-    return res.render("admin/users");
+    const users = readDB('users.json')
+    return res.render("admin/users",{users});
   },
 
   /* BANNERS */
   banners: (req, res) => {
-    return res.render("admin/banners");
+    const banners = readDB('banners.json')
+    return res.render("admin/banners",{banners});
   },
 
   /* DASHBOARD */
